dfb4dc3fb485ee78b7e9d9b51ae55a05b1310a95
Kailash <kailash.doodleblue@gmail.com>
2024-07-05 17:32:29 +0530

    added swagger

--- order-service/index.js
+++ order-service/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const sequelize = require('./config/database');
 const router = require('./routes/orderRoutes');
+const swaggerUI = require('swagger-ui-express');
+const swaggerSpec = require('./config/swagger');
 const dotenv = require('dotenv')
 dotenv.config()
 
 const app = express();
 app.use(express.json());
 app.use('/orders', router);
+app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
 
 const PORT = process.env.PORT || 3002;
 
--- order-service/routes/orderRoutes.js
+++ order-service/routes/orderRoutes.js
@@ -4,9 +4,92 @@ const order = require('../controller/orderController')
 const router = express.Router();
 const {validateOrder,handleValidationErrors}= require('../middleware/validation')
 
-
+/**
+ * @swagger
+ * /orders/:
+ *   get:
+ *     summary: Get all employees
+ *     tags:
+ *       - Orders
+ *     responses:
+ *       200:
+ *         description: A list of employees
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *       401:
+ *         description: Unauthorized
+ */
 router.get('/',order.getallorders );
 
+/**
+ * @swagger
+ * /neworder:
+ *   post:
+ *     summary: Create a new order
+ *     tags:
+ *       - Orders
+ *     requestBody:
+ *       description: Order data to create a new order
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               borrowDate:
+ *                 type: string
+ *                 format: date
+ *                 example: 
+ *               returnDate:
+ *                 type: string
+ *                 format: date
+ *                 example: 
+ *               bookName:
+ *                 type: string
+ *                 example: ""
+ *               price:
+ *                 type: number
+ *                 format: float
+ *                 example: 
+ *               userName:
+ *                 type: string
+ *                 example: ""
+ *     responses:
+ *       200:
+ *         description: Order created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   example: "order_12345"
+ *                 borrowDate:
+ *                   type: string
+ *                   format: date
+ *                   example: 2023-07-01
+ *                 returnDate:
+ *                   type: string
+ *                   format: date
+ *                   example: 2023-07-10
+ *                 bookName:
+ *                   type: string
+ *                   example: "Example Book Name"
+ *                 price:
+ *                   type: number
+ *                   format: float
+ *                   example: 9.99
+ *                 userName:
+ *                   type: string
+ *                   example: "john_doe"
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
 router.post('/neworder',validateOrder,handleValidationErrors,order.addorder );
 
 router.put('/:id',verifytoken,order.updateorder );
